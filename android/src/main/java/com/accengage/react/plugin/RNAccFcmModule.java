71a314cd1bd9228263bf83f3cd5e8ff512cdaab1
Apekka <antoine.pkza@gmail.com>
2019-11-20 16:03:39 +0100

    Updating plugin to 1.3.0. Firebase plugin used is 4.0.4

--- android/src/main/java/com/accengage/react/plugin/RNAccFcmModule.java
+++ android/src/main/java/com/accengage/react/plugin/RNAccFcmModule.java
@@ -1,23 +1,8 @@
 
 package com.accengage.react.plugin;
 
-import android.content.Context;
-import android.util.Log;
-
-import com.ad4screen.sdk.A4SService;
-import com.ad4screen.sdk.Constants;
-import com.ad4screen.sdk.plugins.BasePlugin;
-import com.ad4screen.sdk.plugins.FCMPlugin;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
-import com.ad4screen.sdk.A4S;
-
-import java.util.Locale;
-
-import okhttp3.internal.Util;
-
 
 public class RNAccFcmModule extends ReactContextBaseJavaModule {
 
